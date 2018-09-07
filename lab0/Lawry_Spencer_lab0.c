d537025fca3c618a806539a960649df315429498
spencer.lawry <w58z387@bz-cs-cslab11l.msu.montana.edu>
2018-09-07 11:28:46 -0600

    adding lab0

---
+++ lab0/Lawry_Spencer_lab0.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* Spencer Lawry
+ * Lab0, CSCI 112
+ * 8/29/18
+ */
+
+int main(void){ 
+   // prints "Hello World"
+   printf("Hello World\n");
+   // exit with no errors
+   return(0); // returns False for no error
+}
+
+
+
